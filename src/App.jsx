b637a00809215bb844a00c07e6fb7d9dc2e2eb63
RahulPaul04 <rahulpaul0498@gmail.com>
2024-04-24 00:29:31 +0530

    change in address validation

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,8 @@ function App() {
       }
     }
     else if(name == "address"){
-      if(value.match(/^[a-zA-Z0-9\s.]*[^.]$/
+      if(value.match(/^[a-zA-Z0-9\s.,/()]*[^.,/()]$/
+
 
 
       ) || value == ""){
